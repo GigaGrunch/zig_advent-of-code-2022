e13ea6612b79dcdb913c5f3d7065de1e6254734a
GigaGrunch <gigagrunch@gmail.com>
2022-12-06 00:42:44 +0100

    solve day 2.1 for test input

--- day-2.zig
+++ day-2.zig
@@ -2,14 +2,19 @@ const std = @import("std");
 const input = @embedFile("test-input/day-2.txt");
 
 pub fn main() !void {
+    var total_score: u32 = 0;
+
     var round_it = std.mem.tokenize(u8, input, "\n");
     while (round_it.next()) |round| {
         for (games) |game| {
             if (std.mem.eql(u8, game.string, round)) {
                 std.debug.print("{s} -> {d}\n", .{round, game.score});
+                total_score += game.score;
             }
         }
     }
+
+    std.debug.print("total score = {d}\n", .{total_score});
 }
 
 const games = [_]Game {
