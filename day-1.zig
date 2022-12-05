b758484e7e3a9dafd1c4ecc39cd53077306663fe
GigaGrunch <gigagrunch@gmail.com>
2022-12-05 23:52:10 +0100

    count calories per elf

--- day-1.zig
+++ day-1.zig
@@ -1,14 +1,18 @@
 const std = @import("std");
 const test_input = @embedFile("test-input/day-1.txt");
 
-pub fn main() void {
+pub fn main() !void {
     var elf_it = std.mem.split(u8, test_input, "\n\n");
     while (elf_it.next()) |elf| {
         std.debug.print("elf: ", .{});
+        var total_calories: u32 = 0;
         var item_it = std.mem.split(u8, elf, "\n");
-        while (item_it.next()) |item| {
-            std.debug.print("{s} ", .{item});
+        while (item_it.next()) |calories| {
+            if (calories.len == 0) continue;
+
+            std.debug.print("{s} ", .{calories});
+            total_calories += try std.fmt.parseInt(u32, calories, 10);
         }
-        std.debug.print("\n", .{});
+        std.debug.print("= {d}\n", .{total_calories});
     }
 }
