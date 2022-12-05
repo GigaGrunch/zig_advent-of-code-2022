3d5262764fa7acede39013de0a9a1992276176e5
GigaGrunch <gigagrunch@gmail.com>
2022-12-06 00:40:55 +0100

    identify games individually

--- day-2.zig
+++ day-2.zig
@@ -4,10 +4,27 @@ const input = @embedFile("test-input/day-2.txt");
 pub fn main() !void {
     var round_it = std.mem.tokenize(u8, input, "\n");
     while (round_it.next()) |round| {
-        var move_it = std.mem.tokenize(u8, round, " ");
-        const their_move = move_it.next() orelse unreachable;
-        const my_move = move_it.next() orelse unreachable;
-
-        std.debug.print("{s} -> {s}\n", .{their_move, my_move});
+        for (games) |game| {
+            if (std.mem.eql(u8, game.string, round)) {
+                std.debug.print("found game!\n", .{});
+            }
+        }
     }
 }
+
+const games = [_]Game {
+    .{ .string = "A X", .score = 1 + 3 },
+    .{ .string = "A Y", .score = 2 + 6 },
+    .{ .string = "A Z", .score = 3 + 0 },
+    .{ .string = "B X", .score = 1 + 0 },
+    .{ .string = "B Y", .score = 2 + 3 },
+    .{ .string = "B Z", .score = 2 + 6 },
+    .{ .string = "C X", .score = 3 + 6 },
+    .{ .string = "C Y", .score = 3 + 0 },
+    .{ .string = "C Z", .score = 3 + 3 },
+};
+
+const Game = struct {
+    string: []const u8,
+    score: u32,
+};
