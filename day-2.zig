7fbc7eec2e2d2589d9736afc592849c00c0e4511
GigaGrunch <gigagrunch@gmail.com>
2022-12-06 00:29:24 +0100

    split moves

--- day-2.zig
+++ day-2.zig
@@ -4,6 +4,10 @@ const input = @embedFile("test-input/day-2.txt");
 pub fn main() !void {
     var round_it = std.mem.tokenize(u8, input, "\n");
     while (round_it.next()) |round| {
-        std.debug.print("{s}\n", .{round});
+        var move_it = std.mem.tokenize(u8, round, " ");
+        const their_move = move_it.next() orelse unreachable;
+        const my_move = move_it.next() orelse unreachable;
+
+        std.debug.print("{s} -> {s}\n", .{their_move, my_move});
     }
 }
