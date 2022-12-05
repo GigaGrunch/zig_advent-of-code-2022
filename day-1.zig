96985363af0c472dbd5078e6cf7a2cecf11e551b
GigaGrunch <gigagrunch@gmail.com>
2022-12-05 23:46:25 +0100

    split input by elves and items

--- day-1.zig
+++ day-1.zig
@@ -2,5 +2,13 @@ const std = @import("std");
 const test_input = @embedFile("test-input/day-1.txt");
 
 pub fn main() void {
-    std.debug.print("{s}", .{test_input});
+    var elf_it = std.mem.split(u8, test_input, "\n\n");
+    while (elf_it.next()) |elf| {
+        std.debug.print("elf: ", .{});
+        var item_it = std.mem.split(u8, elf, "\n");
+        while (item_it.next()) |item| {
+            std.debug.print("{s} ", .{item});
+        }
+        std.debug.print("\n", .{});
+    }
 }
