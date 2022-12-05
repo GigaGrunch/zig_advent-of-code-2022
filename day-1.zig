d03d786c70ed1145143f7e87715f2c2098fcd6fd
GigaGrunch <gigagrunch@gmail.com>
2022-12-06 00:02:25 +0100

    solve day 1.2 for test input

--- day-1.zig
+++ day-1.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
-const input = @embedFile("real-input/day-1.txt");
+const input = @embedFile("test-input/day-1.txt");
 
 pub fn main() !void {
-    var max_calories: u32 = 0;
+    var max_calories = [_]u32 {0} ** 3;
 
     var elf_it = std.mem.split(u8, input, "\n\n");
     while (elf_it.next()) |elf| {
@@ -17,8 +17,22 @@ pub fn main() !void {
         }
         std.debug.print("= {d}\n", .{total_calories});
 
-        max_calories = @maximum(max_calories, total_calories);
+        if (total_calories > max_calories[0]) {
+            max_calories[2] = max_calories[1];
+            max_calories[1] = max_calories[0];
+            max_calories[0] = total_calories;
+        }
+        else if (total_calories > max_calories[1]) {
+            max_calories[2] = max_calories[1];
+            max_calories[1] = total_calories;
+        }
+        else if (total_calories > max_calories[2]) {
+            max_calories[2] = total_calories;
+        }
     }
 
-    std.debug.print("max calories = {d}\n", .{max_calories});
+    const total_max_calories = max_calories[0] + max_calories[1] + max_calories[2];
+
+    std.debug.print("max calories = {d} + {d} + {d} = {d}\n",
+        .{max_calories[0], max_calories[1], max_calories[2], total_max_calories});
 }
