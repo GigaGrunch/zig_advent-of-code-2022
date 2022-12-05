12e3574d1b6fee4377b1f7aaa246563c3462f4ec
GigaGrunch <gigagrunch@gmail.com>
2022-12-05 23:54:07 +0100

    solve day 1.1 for test input

--- day-1.zig
+++ day-1.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const test_input = @embedFile("test-input/day-1.txt");
 
 pub fn main() !void {
+    var max_calories: u32 = 0;
+
     var elf_it = std.mem.split(u8, test_input, "\n\n");
     while (elf_it.next()) |elf| {
         std.debug.print("elf: ", .{});
@@ -14,5 +16,9 @@ pub fn main() !void {
             total_calories += try std.fmt.parseInt(u32, calories, 10);
         }
         std.debug.print("= {d}\n", .{total_calories});
+
+        max_calories = @maximum(max_calories, total_calories);
     }
+
+    std.debug.print("max calories = {d}\n", .{max_calories});
 }
