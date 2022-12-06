b1ee61420de8d99717beaea15b3c212e0a2b9347
GigaGrunch <gigagrunch@gmail.com>
2022-12-06 01:28:07 +0100

    solve day 3.2 for test input

--- day-3.zig
+++ day-3.zig
@@ -1,34 +1,45 @@
 const std = @import("std");
-const input = @embedFile("real-input/day-3.txt");
+const input = @embedFile("test-input/day-3.txt");
 
 pub fn main() !void {
     var alloc = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = alloc.deinit();
-    var set = std.AutoHashMap(u8, void).init(alloc.allocator());
-    defer set.deinit();
 
     var total_prio: u32 = 0;
 
     var rucksack_it = std.mem.tokenize(u8, input, "\n");
-    while (rucksack_it.next()) |rucksack| {
-        const compartment_1 = rucksack[0..rucksack.len/2];
-        const compartment_2 = rucksack[rucksack.len/2..];
+    while (rucksack_it.next()) |rucksack_1| {
+        const rucksack_2 = rucksack_it.next() orelse unreachable;
+        const rucksack_3 = rucksack_it.next() orelse unreachable;
 
-        for (compartment_1) |item| try set.put(item, undefined);
-        for (compartment_2) |item| {
-            if (set.contains(item)) {
-                const prio: u32 = switch (item) {
-                    'a'...'z' => item - 'a' + 1,
-                    'A'...'Z' => item - 'A' + 27,
-                    else => unreachable
-                };
-                std.debug.print("{c} -> {d}\n", .{item, prio});
-                total_prio += prio;
-                break;
+        var counts = std.AutoHashMap(u8, u32).init(alloc.allocator());
+        defer counts.deinit();
+
+        const rucksacks = [_][]const u8 { rucksack_1, rucksack_2, rucksack_3 };
+        for (rucksacks) |rucksack| {
+            var rucksack_set = std.AutoHashMap(u8, void).init(alloc.allocator());
+            defer rucksack_set.deinit();
+
+            for (rucksack) |item| try rucksack_set.put(item, undefined);
+
+            var item_it = rucksack_set.keyIterator();
+            while (item_it.next()) |item| {
+                var count = try counts.getOrPut(item.*);
+                count.value_ptr.* = if (count.found_existing) count.value_ptr.* + 1
+                                    else 1;
+
+                if (count.value_ptr.* == 3) {
+                    const prio: u32 = switch (item.*) {
+                        'a'...'z' => item.* - 'a' + 1,
+                        'A'...'Z' => item.* - 'A' + 27,
+                        else => unreachable
+                    };
+                    std.debug.print("{c} -> {d}\n", .{item.*, prio});
+                    total_prio += prio;
+                    break;
+                }
             }
         }
-
-        set.clearAndFree();
     }
 
     std.debug.print("total prio = {d}\n", .{total_prio});
