ee8e046cb89ddf79c8503425fd86647df08f3394
Benjamin Schmithüsen <benjamin.schmithuesen@ravensburger.de>
2022-12-07 12:24:38 +0100

    identify commands, dirs and files

---
+++ day-7.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const input = @embedFile("test-input/day-7.txt");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var alloc = gpa.allocator();
+
+    _ = alloc;
+
+    var lines_it = std.mem.tokenize(u8, input, "\r\n");
+    while (lines_it.next()) |line| {
+        if (line[0] == '$') {
+            std.debug.print("Command: {s}\n", .{line});
+        }
+        else if (line[0] >= '0' and line[0] <= '9') {
+            std.debug.print("File: {s}\n", .{line});
+        }
+        else if (std.mem.eql(u8, line[0..3], "dir")) {
+            std.debug.print("Dir: {s}\n", .{line});
+        }
+        else {
+            std.debug.print("Error at {s}\n", .{line});
+        }
+    }
+}
+
+const Dir = struct {
+    name: []const u8,
+    dirs: std.ArrayList(Dir),
+    files: std.ArrayList(File),
+};
+
+const File = struct {
+    name: []const u8,
+    size: u32,
+};
