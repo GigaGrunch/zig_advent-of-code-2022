1432066695b3b07c8de77b68da87174937c301bc
GigaGrunch <gigagrunch@gmail.com>
2022-12-10 15:05:23 +0100

    day 10: print correct output

--- day-10.zig
+++ day-10.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const input = @embedFile("real-input/day-10.txt");
+const input = @embedFile("test-input/day-10.txt");
 
 var cycle: i32 = 0;
 var x: i32 = 1;
@@ -7,23 +7,33 @@ var x: i32 = 1;
 pub fn main() !void {
     var lines_it = std.mem.tokenize(u8, input, "\r\n");
     while (lines_it.next()) |line| {
+        const v: ?i32 = if (std.mem.eql(u8, line[0..4], "addx"))
+            try std.fmt.parseInt(i32, line[5..], 10)
+            else null;
+
         nextCycle();
 
-        if (std.mem.eql(u8, line[0..4], "addx")) {
-            const v = try std.fmt.parseInt(i32, line[5..], 10);
+        if (v) |value| {
             nextCycle();
-            x += v;
-        }
-        else if (!std.mem.eql(u8, line[0..4], "noop")) {
-            unreachable;
+            x += value;
         }
     }
 }
 
 fn nextCycle() void {
-    cycle += 1;
+    const sprite_pixels = [_]i32 { x-1, x, x+1 };
 
-    std.debug.print(".", .{});
+    var pixel: u8 = '.';
+
+    for (sprite_pixels) |p| {
+        if (@rem(cycle, 40) == p) {
+            pixel = '#';
+        }
+    }
+
+    std.debug.print("{c}", .{pixel});
+
+    cycle += 1;
 
     if (@rem(cycle, 40) == 0) {
         std.debug.print("\n", .{});
