efa42cba5fe463f33a64c5dc60a326a54ae70f3a
GigaGrunch <gigagrunch@gmail.com>
2022-12-10 14:21:04 +0100

    day 10: log x value during the cycles

--- day-10.zig
+++ day-10.zig
@@ -1,24 +1,28 @@
 const std = @import("std");
 const input = @embedFile("test-input/day-10.txt");
 
-pub fn main() !void {
-    var cycle: i32 = 0;
-    var x: i32 = 0;
+var cycle: u32 = 0;
+var x: i32 = 1;
 
+pub fn main() !void {
     var lines_it = std.mem.tokenize(u8, input, "\r\n");
     while (lines_it.next()) |line| {
+        nextCycle();
+
         if (std.mem.eql(u8, line[0..4], "addx")) {
             const v = try std.fmt.parseInt(i32, line[5..], 10);
             x += v;
-            cycle += 2;
+            nextCycle();
         }
-        else if (std.mem.eql(u8, line[0..4], "noop")) {
-            cycle += 1;
-        }
-        else {
-            std.debug.print("Unknown instruction: {s}\n", .{line});
+        else if (!std.mem.eql(u8, line[0..4], "noop")) {
+            unreachable;
         }
     }
+}
 
-    std.debug.print("cycle = {d}, x = {d}\n", .{cycle, x});
+fn nextCycle() void {
+    cycle += 1;
+    if (cycle == 20 or (cycle > 20 and (cycle - 20) % 40 == 0)) {
+        std.debug.print("cycle = {d}, x = {d}\n", .{cycle, x});
+    }
 }
