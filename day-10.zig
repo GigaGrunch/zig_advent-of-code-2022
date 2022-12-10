9fe4e544ae94951ae44515b530274719c840472e
GigaGrunch <gigagrunch@gmail.com>
2022-12-10 14:39:38 +0100

    day 10: solve for test input

--- day-10.zig
+++ day-10.zig
@@ -1,28 +1,37 @@
 const std = @import("std");
 const input = @embedFile("test-input/day-10.txt");
 
-var cycle: u32 = 0;
+var cycle: i32 = 0;
 var x: i32 = 1;
+var signal_sum: i32 = 0;
 
 pub fn main() !void {
     var lines_it = std.mem.tokenize(u8, input, "\r\n");
+    var line_number: usize = 1;
     while (lines_it.next()) |line| {
-        nextCycle();
+        nextCycle(line_number);
 
         if (std.mem.eql(u8, line[0..4], "addx")) {
             const v = try std.fmt.parseInt(i32, line[5..], 10);
+            nextCycle(line_number);
             x += v;
-            nextCycle();
         }
         else if (!std.mem.eql(u8, line[0..4], "noop")) {
             unreachable;
         }
+
+        line_number += 1;
     }
+
+    std.debug.print("signal sum = {d}\n", .{signal_sum});
 }
 
-fn nextCycle() void {
+fn nextCycle(line_number: usize) void {
     cycle += 1;
-    if (cycle == 20 or (cycle > 20 and (cycle - 20) % 40 == 0)) {
-        std.debug.print("cycle = {d}, x = {d}\n", .{cycle, x});
+
+    const signal = cycle * x;
+    if (cycle == 20 or @rem(cycle - 20, 40) == 0) {
+        signal_sum += signal;
+        std.debug.print("{d}: cycle = {d}, x = {d}, signal = {d}\n", .{line_number, cycle, x, signal});
     }
 }
