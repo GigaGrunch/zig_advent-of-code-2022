0784ca7fe8394902cd0154c37fbdfb8b2bf8a454
GigaGrunch <gigagrunch@gmail.com>
2023-12-02 22:02:55 +0100

    parse all list pairs

--- day13/day13.zig
+++ day13/day13.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+var lists: std.ArrayList(*List) = undefined;
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -20,8 +22,109 @@ pub fn main() !void {
 
     std.debug.print("file has {d} bytes\n", .{text.len});
 
+    lists = std.ArrayList(*List).init(gpa.allocator());
+    defer lists.deinit();
+
     var lines = std.mem.tokenizeAny(u8, text, "\r\n");
-    while (lines.next()) |line| {
-        std.debug.print("{s}\n", .{line});
+    while (lines.next()) |first_line| {
+        var root_1: *List = undefined;
+        {
+            var parts = ValueIterator.init(first_line);
+            std.debug.assert(std.mem.eql(u8, parts.next().?, "["));
+            root_1 = try parseList(&parts);
+        }
+        var root_2: *List = undefined;
+        {
+            const second_line = lines.next().?;
+
+            var parts = ValueIterator.init(second_line);
+            std.debug.assert(std.mem.eql(u8, parts.next().?, "["));
+            root_2 = try parseList(&parts);
+        }
+
+        printList(root_1);
+        std.debug.print("\n", .{});
+        printList(root_2);
+        std.debug.print("\n\n", .{});
+
+        for (lists.items) |list| {
+            list.deinit();
+            gpa.allocator().destroy(list);
+        }
+        lists.clearRetainingCapacity();
     }
 }
+
+fn parseList(parts: *ValueIterator) !*List {
+    var list = try lists.allocator.create(List);
+    list.* = List.init(lists.allocator);
+    try lists.append(list);
+
+    while (parts.next()) |part| {
+        if (std.mem.eql(u8, part, "[")) {
+            try list.append(.{.list = try parseList(parts)});
+        } else if (std.mem.eql(u8, part, "]")) {
+            return list;
+        } else {
+            try list.append(.{.number = try std.fmt.parseInt(i32, part, 10)});
+        }
+    }
+
+    return list;
+}
+
+fn deinitList(list: *List) void {
+    for (list.items) |value| {
+        switch (value) {
+            .number => { },
+            .list => |sub_list| deinitList(sub_list),
+        }
+    }
+
+    list.deinit();
+}
+
+fn printList(list: *List) void {
+    std.debug.print("[", .{});
+    for (list.items) |value| {
+        std.debug.print(" ", .{});
+        switch (value) {
+            .number => |number| std.debug.print("{d}", .{number}),
+            .list => |sub_list| printList(sub_list),
+        }
+        std.debug.print(" ", .{});
+    }
+    std.debug.print("]", .{});
+}
+
+const ValueIterator = struct {
+    string: []const u8,
+
+    pub fn init(string: []const u8) ValueIterator {
+        return .{ .string = string };
+    }
+
+    pub fn next(self: *ValueIterator) ?[]const u8 {
+        if (self.string.len == 0) return null;
+        while (self.string[0] == ',') self.string = self.string[1..];
+        if (self.string[0] == '[') {
+            self.string = self.string[1..];
+            return "[";
+        }
+        if (self.string[0] == ']') {
+            self.string = self.string[1..];
+            return "]";
+        }
+        
+        const end_index = std.mem.indexOfAny(u8, self.string, ",[]") orelse self.string.len;
+        defer self.string = self.string[end_index..];
+        return self.string[0..end_index];
+    }
+};
+
+const Value = union(enum) {
+    number: i32,
+    list: *List,
+};
+
+const List = std.ArrayList(Value);
